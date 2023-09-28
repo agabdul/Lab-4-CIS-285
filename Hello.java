b7077a85b0a9fbc9bd60da9c1603aebf7e4d4fb4
doom-mi <dominicver@hotmail.com>
2023-09-28 16:34:37 -0400

    This is a test of forking

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[]args){
-
+// This is your friend, Dominic
         for(int i=1; i<=10; i++){
             System.out.println("Hello World Have a Good Day... run: " + i);
         }
